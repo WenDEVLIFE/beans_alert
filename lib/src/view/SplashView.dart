1e944c2a922bd3ff7ac799e30192a16bcdde000c
wendevlife <frouenmedinajr@gmail.com>
2025-07-28 13:52:41 +0800

    Added bloc and implment it on the main.dart

--- lib/src/view/SplashView.dart
+++ lib/src/view/SplashView.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
-
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  _SplashViewState createState() => _SplashViewState();
+
+}
+
+class _SplashViewState extends State<SplashView> {
 
   @override
   Widget build(BuildContext context) {
