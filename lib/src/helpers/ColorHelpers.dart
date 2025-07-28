ee48a547e3e9dff5b414c89f2ec385b0d7abd62e
wendevlife <frouenmedinajr@gmail.com>
2025-07-28 14:17:49 +0800

    Added ColorHelpers.dart and added the colors

---
+++ lib/src/helpers/ColorHelpers.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class ColorHelpers {
+
+  static const Color primaryColor = Color(0xFF191816);
+  static const Color secondaryColor = Color(0xFFd7cebe);
+  static const Color accentColor = Color(0xFFfc302d);
+}
