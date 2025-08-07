0e6400c8d191bc2db350104540122ed2a5f5d6b4
wendevlife <frouenmedinajr@gmail.com>
2025-08-07 10:50:31 +0800

    Added SvgHelpers.dart and implement the ico

---
+++ lib/src/helpers/SvgHelpers.dart
@@ -0,0 +1,8 @@
+class SvgHelpers {
+  static const String phonebook = 'assets/ico/bookuser.svg';
+  static const String calendar = 'assets/ico/calendar.svg';
+  static const String email = 'assets/ico/mail.svg';
+  static const String addEmail = 'assets/ico/mail_plus.svg';
+  static const String userSettings = 'assets/ico/user_settings.svg';
+
+}
