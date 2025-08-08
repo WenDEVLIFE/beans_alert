1a9c5d178bae83207c2a92a761ec1aecb22306fd
wendevlife <frouenmedinajr@gmail.com>
2025-08-08 14:41:28 +0800

    Added new menus on the drawer

--- lib/src/widget/CustomNavigationSideBar.dart
+++ lib/src/widget/CustomNavigationSideBar.dart
@@ -1,43 +1,138 @@
+import 'package:beans_alert/src/helpers/ColorHelpers.dart';
+import 'package:beans_alert/src/helpers/SvgHelpers.dart';
+import 'package:beans_alert/src/widget/CustomText.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../helpers/ImageHelper.dart';
 
 class CustomNavigationSideBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Drawer(
+      backgroundColor: ColorHelpers.primaryColor,
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
           DrawerHeader(
             decoration: BoxDecoration(
-              color: Colors.blue,
+              color: ColorHelpers.primaryColor,
             ),
-            child: Text(
-              'Navigation',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
+            child: Center(
+              child:  Container(
+                width: screenWidth * 0.9,
+                height: screenHeight * 0.3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(ImageHelper.logoPath),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            )
+          ),
+          ListTile(
+            leading: SvgPicture.asset(
+              SvgHelpers.userSettings,
+              width: 24,
+              height: 24,
+              colorFilter: ColorFilter.mode(
+                Colors.white,
+                BlendMode.srcIn,
               ),
             ),
-          ),
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text('Home'),
+            title: CustomText(text: 'User Management',
+                fontFamily: 'Anton',
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+                textAlign: TextAlign.left),
             onTap: () {
               Navigator.pop(context);
               // Add navigation logic here
             },
           ),
           ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('Settings'),
+            leading: SvgPicture.asset(
+              SvgHelpers.addEmail,
+              width: 24,
+              height: 24,
+              colorFilter: ColorFilter.mode(
+                Colors.white,
+                BlendMode.srcIn,
+              ),
+            ),
+            title: CustomText(text: 'Send Message',
+            fontFamily: 'Anton',
+            fontSize: 20,
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            textAlign: TextAlign.left),
             onTap: () {
               Navigator.pop(context);
               // Add navigation logic here
             },
           ),
           ListTile(
-            leading: Icon(Icons.logout),
-            title: Text('Logout'),
+            leading: SvgPicture.asset(
+              SvgHelpers.email,
+              width: 24,
+              height: 24,
+              colorFilter: ColorFilter.mode(
+                Colors.white,
+                BlendMode.srcIn,
+              ),
+            ),
+            title: CustomText(text: 'Message History',
+                fontFamily: 'Anton',
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+                textAlign: TextAlign.left),
+            onTap: () {
+              Navigator.pop(context);
+              // Add logout logic here
+            },
+          ),
+          ListTile(
+            leading: SvgPicture.asset(
+              SvgHelpers.phonebook,
+              width: 24,
+              height: 24,
+              colorFilter: ColorFilter.mode(
+                Colors.white,
+                BlendMode.srcIn,
+              ),
+            ),
+            title: CustomText(text: 'Contacts',
+                fontFamily: 'Anton',
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+                textAlign: TextAlign.left),
+            onTap: () {
+              Navigator.pop(context);
+              // Add logout logic here
+            },
+          ),
+          ListTile(
+            leading: SvgPicture.asset(
+              SvgHelpers.calendar,
+              width: 24,
+              height: 24,
+              colorFilter: ColorFilter.mode(
+                Colors.white,
+                BlendMode.srcIn,
+              ),
+            ),
+            title: CustomText(text: 'Calendar',
+                fontFamily: 'Anton',
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+                textAlign: TextAlign.left),
             onTap: () {
               Navigator.pop(context);
               // Add logout logic here
