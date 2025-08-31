42aeb7c00cccc741b7176af264cf1a53026df535
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-08-31 17:36:50 +0800

    Added FirebaseService.dart for ios

--- lib/src/services/FirebaseService.dart
+++ lib/src/services/FirebaseService.dart
@@ -1,11 +1,15 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 
 class FirebaseServices {
   static Future<void> run() async {
     await Firebase.initializeApp(
-      options: const FirebaseOptions(
+      options: FirebaseOptions(
         apiKey: 'AIzaSyCGdshUog0aSImICPDs1epYFuaJbTqCAHU',
-        appId: '1:671405515090:android:08cd2a72403132074c5a8b',
+        appId: Platform.isIOS
+            ?  '1:671405515090:ios:c34326a9fd8523264c5a8b'
+            : '1:671405515090:android:08cd2a72403132074c5a8b',
         messagingSenderId: '671405515090',
         projectId: 'beans-alert-database',
         storageBucket: 'beans-alert-database.firebasestorage.app',
